39e26b6e421083f7927c119dfc698bd27ffff591
ishkraj <ishkrajansari.690@gmail.com>
2026-06-13 17:35:47 +0530

    Fixed CORS for production

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 // ===========================================
 // 👇 MIDDLEWARE (Ye SABSE PEHLE aana chahiye)
 // ===========================================
-app.use(cors());
+app.use(cors({
+  origin: ["https://magikchef-official.vercel.app", "https://magikchef.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); // FormData texts ko support karne ke liye
 
